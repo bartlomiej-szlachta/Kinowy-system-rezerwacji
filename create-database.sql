61b08db0529e203dd67ea4bd02c1e0896960bf7f
Kamil Rusin <lotrusek@gmail.com>
2019-06-15 18:24:04 +0200

    Poprawki językowe i zaktualizowana tabela Miejsca oraz Uzytkownicy

--- create-database.sql
+++ create-database.sql
@@ -1,20 +1,19 @@
-CREATE DATABASE Kino;
+CREATE DATABASE KINOWY_SYSTEM_REZERWACJI;
 
-USE Kino;
+USE KINOWY_SYSTEM_REZERWACJI;
 
 CREATE TABLE Miejsca(
 id int unsigned not null auto_increment,
 rzad tinyint unsigned not null,
 numer tinyint unsigned not null,
-stan enum('wolny','zajety') not null,
 PRIMARY KEY(id))
 ENGINE = innodb
 default character set utf8 collate utf8_unicode_ci;
 
 CREATE TABLE Uzytkownicy(
 id int unsigned not null auto_increment,
-username char(14) not null,
-hashed_password char(50),
+nazwa_uzytkownika char(14) not null,
+ukryte_haslo char(50),
 imie char(20) not null,
 nazwisko char(30) not null,
 email char(30) not null,
@@ -33,17 +32,6 @@ PRIMARY KEY(id))
 ENGINE = innodb
 default character set utf8 collate utf8_unicode_ci;
 
-CREATE TABLE Rezerwacje(
-id int unsigned not null auto_increment,
-id_seansu int unsigned not null,
-id_uzytkownika int unsigned not null,
-id_miejsca int unsigned not null,
-PRIMARY KEY(id),
-CONSTRAINT fk_rezerwacjauzytkownika FOREIGN KEY (id_uzytkownika) REFERENCES Uzytkownicy(id),
-CONSTRAINT fk_rezerwacjamiejsc FOREIGN KEY (id_miejsca) REFERENCES Miejsca(id))
-ENGINE = innodb
-default character set utf8 collate utf8_unicode_ci;
-
 CREATE TABLE Seanse(
 id int unsigned not null auto_increment,
 id_filmu int unsigned not null,
@@ -53,5 +41,15 @@ CONSTRAINT fk_projekcja FOREIGN KEY(id_filmu) REFERENCES Filmy(id))
 ENGINE = innodb
 default character set utf8 collate utf8_unicode_ci;
 
-ALTER TABLE Rezerwacje
-ADD CONSTRAINT fk_rezerwacjawseansie FOREIGN KEY (id_seansu) REFERENCES Seanse(id);
+CREATE TABLE Rezerwacje(
+id int unsigned not null auto_increment,
+id_seansu int unsigned not null,
+id_uzytkownika int unsigned not null,
+id_miejsca int unsigned not null,
+PRIMARY KEY(id),
+CONSTRAINT fk_rezerwacjauzytkownika FOREIGN KEY (id_uzytkownika) REFERENCES Uzytkownicy(id),
+CONSTRAINT fk_rezerwacjamiejsc FOREIGN KEY (id_miejsca) REFERENCES Miejsca(id),
+CONSTRAINT fk_rezerwacjawseansie FOREIGN KEY (id_seansu) REFERENCES Seanse(id))
+ENGINE = innodb
+default character set utf8 collate utf8_unicode_ci
+ 
