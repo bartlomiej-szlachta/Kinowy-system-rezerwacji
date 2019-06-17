4cd163e3193686b7aee15ddd19f0a7deafacf81e
Kamil Rusin <lotrusek@gmail.com>
2019-06-17 23:28:19 +0200

    Dodano klasy dla encji z tabel w folderze model

---
+++ KinowySystemRezerwacji/service/model/MiejsceRezerwacjaEntity.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace KinowySystemRezerwacji.service.model
+{
+    /// <summary>
+    /// Klasa reprezentująca encję miejsca_rezerwacje z bazy danych.
+    /// </summary>
+    class MiejsceRezerwacjaEntity
+    {
+        /// <summary>
+        /// Id miejsca_rezerwacje.
+        /// </summary>
+        internal int Id { get; set; }
+
+        /// <summary>
+        /// Id rezerwacji.
+        /// </summary>
+        internal int Id_rezerwacji { get; set; }
+
+        /// <summary>
+        /// Id miejsca.
+        /// </summary>
+        internal int Id_miejsca { get; set; }
+
+        /// <summary>
+        /// Konstruktor.
+        /// </summary>
+        /// <param name="reader">Obiekt readera MySql</param>
+        internal MiejsceRezerwacjaEntity(MySqlDataReader reader)
+        {
+            Id = (int)reader["id"];
+            Id_rezerwacji = (int)reader["id_rezerwacji"];
+            Id_miejsca = (int)reader["id_miejsca"];
+        }
+    }
+}
---
+++ KinowySystemRezerwacji/service/model/UzytkownikEntity.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace KinowySystemRezerwacji.service.model
+{
+    /// <summary>
+    /// Klasa reprezentująca encję użytkownika z bazy danych.
+    /// </summary>
+    internal class UzytkownikEntity
+    {
+        /// <summary>
+        /// Id użytkownika.
+        /// </summary>
+        internal int Id { get; set; }
+
+        /// <summary>
+        /// Nazwa użytkownika.
+        /// </summary>
+        internal string Nazwa_uzytkownika { get; set; }
+
+        /// <summary>
+        /// Ukryte hasło użytkownika.
+        /// </summary>
+        internal string Ukryte_haslo { get; set; }
+
+        /// <summary>
+        /// Imię użytkownika.
+        /// </summary>
+        internal string Imie { get; set; }
+
+        /// <summary>
+        /// Nazwisko użytkownika.
+        /// </summary>
+        internal string Nazwisko { get; set; }
+
+        /// <summary>
+        /// Email użytkownika.
+        /// </summary>
+        internal string Email { get; set; }
+
+        /// <summary>
+        /// Data urodzenia użytkownika.
+        /// </summary>
+        internal DateTime Data_urodzenia { get; set; }
+
+        /// <summary>
+        /// Konstruktor.
+        /// </summary>
+        /// <param name="reader">Obiekt readera MySql</param>
+        internal UzytkownikEntity(MySqlDataReader reader)
+        {
+            Id = (int)reader["id"];
+            Nazwa_uzytkownika = (string)reader["nazwa_uzytkownika"];
+            Ukryte_haslo = (string)reader["ukryte_haslo"];
+            Imie = (string)reader["imie"];
+            Nazwisko = (string)reader["nazwisko"];
+            Email = (string)reader["email"];
+            Data_urodzenia = (DateTime)reader["date"];
+        }
+    }
+}
