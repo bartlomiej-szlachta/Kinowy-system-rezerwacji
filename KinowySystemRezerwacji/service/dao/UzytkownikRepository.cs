a8da021ca4bee0a1817150eec00fae0e14d82616
Kamil Rusin <lotrusek@gmail.com>
2019-06-25 13:34:58 +0200

    Merge z mastera Dodano funkcje Save oraz ExistsByNazwaUzytkownika Poprawiono nazwę kolumny data_urodzenia w tworzeniu instancji obiektu UzytkownikEntity

---
+++ KinowySystemRezerwacji/service/dao/UzytkownikRepository.cs
@@ -0,0 +1,63 @@
+﻿using KinowySystemRezerwacji.service.model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinowySystemRezerwacji.service.dao
+{
+    /// <summary>
+    /// Repozytorium użytkowników.
+    /// </summary>
+    internal class UzytkownikRepository
+    {
+        /// <summary>
+        /// Metoda zapisująca użytkownika do bazy danych.
+        /// <param name="uzytkownik">Encja użytkownika, którego zapiszemy do bazy</param>
+        /// </summary>
+        internal void Save(UzytkownikEntity uzytkownik)
+        {
+            MySqlConnection connection = DBConnection.Instance.Connection;
+            connection.Open();
+            MySqlCommand insertingCommand = connection.CreateCommand();
+            insertingCommand.CommandText = "INSERT INTO Uzytkownicy(nazwa_uzytkownika, ukryte_haslo, imie, nazwisko, email, data_urodzenia) VALUES (?nazwa_uzytkownika, ?ukryte_haslo, ?imie, ?nazwisko, ?email, ?data_urodzenia)";
+            insertingCommand.Parameters.AddWithValue("?nazwa_uzytkownika", uzytkownik.Nazwa_uzytkownika);
+            insertingCommand.Parameters.AddWithValue("?ukryte_haslo", uzytkownik.Ukryte_haslo);
+            insertingCommand.Parameters.AddWithValue("?imie", uzytkownik.Imie);
+            insertingCommand.Parameters.AddWithValue("?nazwisko", uzytkownik.Nazwisko);
+            insertingCommand.Parameters.AddWithValue("?email", uzytkownik.Email);
+            insertingCommand.Parameters.AddWithValue("?data_urodzenia", uzytkownik.Data_urodzenia.ToString("yyyy-MM-dd"));
+            insertingCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        /// <summary>
+        /// Metoda sprawdzająca w bazie danych czy użytkownik o takiej nazwie istnieje.
+        /// </summary>
+        /// <param name="username">Nazwa użytkownika, którą sprawdzamy</param>
+        /// <returns>Prawda/Fałsz</returns>
+        internal bool ExistsByNazwaUzytkownika(string username)
+        {
+            MySqlConnection connection = DBConnection.Instance.Connection;
+            using (MySqlCommand command = new MySqlCommand("SELECT id FROM Uzytkownicy WHERE nazwa_uzytkownika = '" + username + "';", connection))
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (Convert.IsDBNull(reader["id"]))
+                {
+                    connection.Close();
+                    return false;
+                }
+                else
+                {
+                    connection.Close();
+                    return true;
+                }
+                
+            }
+        }
+    }
+}
--- KinowySystemRezerwacji/service/model/UzytkownikEntity.cs
+++ KinowySystemRezerwacji/service/model/UzytkownikEntity.cs
@@ -55,7 +55,7 @@ namespace KinowySystemRezerwacji.service.model
             Imie = (string)reader["imie"];
             Nazwisko = (string)reader["nazwisko"];
             Email = (string)reader["email"];
-            Data_urodzenia = (DateTime)reader["date"];
+            Data_urodzenia = (DateTime)reader["data_urodzenia"];
         }
     }
 }
