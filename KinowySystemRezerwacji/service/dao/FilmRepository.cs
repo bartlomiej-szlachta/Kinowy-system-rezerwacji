ffaf35fcdc641556c4d63bea35199a34da49af74
Kamil Rusin <lotrusek@gmail.com>
2019-06-30 21:24:31 +0200

    Dodano FilmRepository oraz SeansRepository wraz z wymaganymi funkcjami

---
+++ KinowySystemRezerwacji/service/dao/FilmRepository.cs
@@ -0,0 +1,38 @@
+﻿using KinowySystemRezerwacji.service.model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinowySystemRezerwacji.service.dao
+{
+    /// <summary>
+    /// Repozytorium filmów.
+    /// </summary>
+    internal class FilmRepository
+    {
+        /// <summary>
+        /// Metoda wyszukuje w bazie danych film o wybranym id.
+        /// </summary>
+        /// <param name="id">Id filmu, którego szukamy</param>
+        /// <returns>Zwraca encję filmu wyszukanego w bazie</returns>
+        internal FilmEntity FindById(int id)
+        {
+            FilmEntity movie = null;
+            MySqlConnection connection = DBConnection.Instance.Connection;
+            using (MySqlCommand command = new MySqlCommand("SELECT * FROM filmy f WHERE f.id = '" + id + "';", connection))
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    movie = new FilmEntity(reader);
+                }
+                connection.Close();
+            }
+            return movie;
+        }
+    }
+}
---
+++ KinowySystemRezerwacji/service/dao/SeansRepository.cs
@@ -0,0 +1,37 @@
+﻿using KinowySystemRezerwacji.service.model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinowySystemRezerwacji.service.dao
+{
+    /// <summary>
+    /// Repozytorium seansów.
+    /// </summary>
+    internal class SeansRepository
+    {
+        /// <summary>
+        /// Metoda wyciągająca z bazy danych dane dotyczące wszystkich seansów w danym dniu.
+        /// </summary>
+        /// <returns>Lista seansów</returns>
+        internal List<SeansEntity> FindByKiedy(DateTime kiedy)
+        {
+            List<SeansEntity> projections = new List<SeansEntity>();
+            MySqlConnection connection = DBConnection.Instance.Connection;
+            using (MySqlCommand command = new MySqlCommand("SELECT * FROM seanse WHERE DATE(kiedy) = '" + kiedy.ToString("yyyy-MM-dd") + "';", connection))
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    projections.Add(new SeansEntity(reader));
+                }
+                connection.Close();
+            }
+            return projections;
+        }
+    }
+}
