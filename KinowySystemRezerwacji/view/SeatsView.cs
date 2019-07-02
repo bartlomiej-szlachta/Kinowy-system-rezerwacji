d98a6c26279859e99e375941f4deceb636ad6769
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-07-02 19:09:21 +0200

    [wip] rozpoczęcie pracy

---
+++ KinowySystemRezerwacji/view/SeatsView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KinowySystemRezerwacji.dto;
+
+namespace KinowySystemRezerwacji.view
+{
+    public partial class SeatsView : UserControl
+    {
+        private List<Label> labels;
+        private const int SPACES_SIZE = 5;
+
+        public SeatsView()
+        {
+            InitializeComponent();
+        }
+
+        internal SeatToChooseResponse[] Seats
+        {
+            set
+            {
+                labels = new List<Label>();
+                foreach (SeatToChooseResponse seat in value)
+                {
+                    Label label = new Label();
+
+                    label.Height;
+                    label.Width;
+                    label.BackColor;
+                    label.Top;
+                    label.Left;
+
+                    labels.Add(label);
+                }
+            }
+        }
+
+    }
+}
