dd9c4ce15c1f3e19b3476b2f75549ff014ec396a
Kamil Rusin <lotrusek@gmail.com>
2019-07-02 23:06:44 +0200

    Dodano pogrubienie dni, w których są seanse

--- KinowySystemRezerwacji/view/ExtendedMonthCalendar.cs
+++ KinowySystemRezerwacji/view/ExtendedMonthCalendar.cs
@@ -31,7 +31,7 @@ namespace KinowySystemRezerwacji.view
         {
             set
             {
-                throw new NotImplementedException();
+                monthCalendar.BoldedDates = value;
             }
         }
 
