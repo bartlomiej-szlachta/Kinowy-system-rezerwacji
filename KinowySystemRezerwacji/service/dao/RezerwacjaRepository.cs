bd15b471c733d05d428e7eccf5687d5e2f2f9198
Bartłomiej Szlachta <37980016+bartlomiej-szlachta@users.noreply.github.com>
2019-07-03 08:21:43 +0200

    Utworzenie funkcjonalności listy wcześniejszych rezerwacji (#43)

--- KinowySystemRezerwacji/service/dao/RezerwacjaRepository.cs
+++ KinowySystemRezerwacji/service/dao/RezerwacjaRepository.cs
@@ -34,5 +34,27 @@ namespace KinowySystemRezerwacji.service.dao
             }
             return rezerwacje;
         }
+
+        /// <summary>
+        /// Metoda wyciągająca z bazy danych dane rezerwacji wykonancych przez danego użytkownika.
+        /// </summary>
+        /// <param name="idUzytkownika">ID użytkownika</param>
+        /// <returns>Dane rezerwacji</returns>
+        internal List<RezerwacjaEntity> FindAllByUzytkownikId(int idUzytkownika)
+        {
+            List<RezerwacjaEntity> rezerwacje = new List<RezerwacjaEntity>();
+            MySqlConnection connection = DBConnection.Instance.Connection;
+            using (MySqlCommand command = new MySqlCommand($"SELECT * FROM rezerwacje WHERE id_uzytkownika = {idUzytkownika};", connection))
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    rezerwacje.Add(new RezerwacjaEntity(reader));
+                }
+                connection.Close();
+            }
+            return rezerwacje;
+        }
     }
 }
